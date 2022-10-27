46003e3358b05bcceadd83d06a24e38b9ead7475
cesar gonzalez <ce.gonzalezv@duocuc.cl>
2022-10-27 07:21:55 -0300

    Experiencia 2

--- src/app/pages/escanear/escanear.page.spec.ts
+++ src/app/pages/escanear/escanear.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { EscanearPage } from './escanear.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('EscanearPage', () => {
+  let component: EscanearPage;
+  let fixture: ComponentFixture<EscanearPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ EscanearPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(EscanearPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
